c428f14908cf74f48fb1df4897a85b31d0a7796f
Vishal Payyawar <vishal.payyawar22@vit.edu>
2025-09-03 11:38:46 +0530

    update login

--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,9 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <h1 style={{ textAlign: "center", marginTop: "50px" }}>Lift State Up Demo</h1>
+      <h1 style={{ textAlign: "center", marginTop: "50px" }}>Parent Component</h1>
       {isLoggedIn ? (
-        <h2 style={{ textAlign: "center" }}>Welcome! You are logged in ✅</h2>
+        <h2 style={{ textAlign: "center" }}>You are logged in!</h2>
       ) : (
         <LoginForm isLoggedIn={isLoggedIn} onLogin={handleLogin} />
       )}
